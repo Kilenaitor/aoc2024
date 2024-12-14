5f163dff9c2e89c5f6d34e57e4e609516bc3f3c8
Kyle Minshall <kyleminshall@gmail.com>
2024-12-14 01:31:14 -0800

    Day 7 part 1

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -1,13 +1,51 @@
 use crate::days::day::Day;
+use std::collections::VecDeque;
 
 pub struct Day7 {}
 
-impl Day7 {}
+impl Day7 {
+    fn parse_input(&self, line: &str) -> (f64, VecDeque<u32>) {
+        let (total_string, numbers_string) = line.split_once(": ").unwrap();
+        let mut numbers = numbers_string
+            .split(" ")
+            .map(|number| number.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+        numbers.reverse();
+        (total_string.parse::<f64>().unwrap(), numbers.into())
+    }
+
+    fn can_make_total(&self, remaining: f64, mut remaining_numbers: VecDeque<u32>) -> bool {
+        if remaining_numbers.len() == 0 {
+            remaining == 0.0
+        } else {
+            let next_value = remaining_numbers.pop_front().unwrap();
+            if next_value as f64 > remaining {
+                false
+            } else {
+                self.can_make_total(remaining - next_value as f64, remaining_numbers.clone())
+                    || self.can_make_total(remaining / next_value as f64, remaining_numbers.clone())
+            }
+        }
+    }
+}
 
 impl Day for Day7 {
     fn part1(&self, input: &str) {
+        let result = input
+            .lines()
+            .map(|line| {
+                let (total, numbers) = self.parse_input(line);
+                if self.can_make_total(total, numbers.clone()) {
+                    total as u64
+                } else {
+                    0
+                }
+            })
+            .reduce(|total, next| total + next)
+            .unwrap();
+
+        println!("{}", result);
     }
 
-    fn part2(&self, input: &str) {
-    }
+    fn part2(&self, input: &str) {}
 }
